5b35897859653fb43963aa9a55689451459f1a1a
wolterlp <walter81w@hotmail.es>
2024-10-05 17:42:42 -0500

    TablasPrincipales

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
 }
--- database/migrations/2024_10_05_175138_create_lessons_table.php
+++ database/migrations/2024_10_05_175138_create_lessons_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->text('description')->max(400);
+            $table->string('image_uri', 255)->nullable();
+            $table->string('content_uri', 255);
+            $table->string('pdf_uri', 255);
+            $table->unsignedBigInteger('level_id');
             $table->timestamps();
+
+            
+            $table->foreign('level_id')->references('id')->on('levels')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
